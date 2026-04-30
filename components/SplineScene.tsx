1f439e8d023284a1fd50bcea08ce5042ea3d3f1b
mabdullah-ismail <mabdullah-ismail@users.noreply.github.com>
2026-05-01 03:44:20 +0500

    fix: revert to standard react-spline import to fix missing 3D model

--- components/SplineScene.tsx
+++ components/SplineScene.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import Spline from '@splinetool/react-spline/next';
+import Spline from '@splinetool/react-spline';
 
 export default function SplineScene() {
   return (
     <div className="spline-container">
       <Spline
         scene="https://prod.spline.design/CZ5FWh4w2dkU889E/scene.splinecode"
+        style={{ width: '100%', height: '100%' }}
       />
     </div>
   );
