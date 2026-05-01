c57adba4d717e407092bc100fd141aa41ed00c4e
mabdullah-ismail <mabdullah-ismail@users.noreply.github.com>
2026-05-01 05:07:31 +0500

    feat: use local cleaned spline file to remove watermark

--- components/SplineScene.tsx
+++ components/SplineScene.tsx
@@ -7,7 +7,7 @@ export default function SplineScene() {
   return (
     <Suspense fallback={null}>
       <Spline
-        scene="https://prod.spline.design/CZ5FWh4w2dkU889E/scene.splinecode"
+        scene="/scene.splinecode"
         className="w-full h-full"
       />
     </Suspense>
